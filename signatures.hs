7f86c898d2ecfcab5edbef47deb353a8c1d41747
Gregory Reshetniak <gregory.reshetniak@nokia.com>
2017-09-07 00:04:30 +0200

    guards

--- signatures.hs
+++ signatures.hs
@@ -2,7 +2,9 @@ functionH :: [a] -> a
 functionH (x:_) = x
 
 functionC :: Ord a => a -> a -> Bool
-functionC x y = if (x > y) then True else False
+functionC x y
+	| (x > y)   = True 
+	| otherwise = False
 
 functionS :: (a,b) -> b
 functionS (x, y) = y
